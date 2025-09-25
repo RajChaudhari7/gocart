faac835fd00d4d9d6fa66d5c5af71055de8092b0
RajChaudhari7 <rajchaudharii1403@gmail.com>
2025-09-25 17:58:47 +0530

    completed project

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -14,10 +14,10 @@ const Footer = () => {
         {
             title: "PRODUCTS",
             links: [
-                { text: "Earphones", path: '/', icon: null },
-                { text: "Headphones", path: '/', icon: null },
-                { text: "Smartphones", path: '/', icon: null },
-                { text: "Laptops", path: '/', icon: null },
+                { text: "Earphones", path: '/shop', icon: null },
+                { text: "Headphones", path: '/shop', icon: null },
+                { text: "Smartphones", path: '/shop', icon: null },
+                { text: "Laptops", path: '/shop', icon: null },
             ]
         },
         {
@@ -32,9 +32,9 @@ const Footer = () => {
         {
             title: "CONTACT",
             links: [
-                { text: "+1-212-456-7890", path: '/', icon: MailIcon },
-                { text: "contact@example.com", path: '/', icon: PhoneIcon },
-                { text: "794 Francisco, 94102", path: '/', icon: MapPinIcon }
+                { text: "globalmart@gmail.com", path: '/', icon: MailIcon },
+                { text: "+91-8600412566", path: '/', icon: PhoneIcon },
+                { text: "Nandurbar, Maharashtra-425412", path: '/', icon: MapPinIcon }
             ]
         }
     ];
@@ -52,9 +52,9 @@ const Footer = () => {
                 <div className="flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b border-slate-500/30 text-slate-500">
                     <div>
                         <Link href="/" className="text-4xl font-semibold text-slate-700">
-                            <span className="text-green-600">go</span>cart<span className="text-green-600 text-5xl leading-0">.</span>
+                            <span className="text-green-600">Global</span>Mart<span className="text-green-600 text-5xl leading-0">.</span>
                         </Link>
-                        <p className="max-w-[410px] mt-6 text-sm">Welcome to gocart, your ultimate destination for the latest and smartest gadgets. From smartphones and smartwatches to essential accessories, we bring you the best in innovation — all in one place.</p>
+                        <p className="max-w-[410px] mt-6 text-sm">Welcome to globalmart, your ultimate destination for the latest and smartest gadgets. From smartphones and smartwatches to essential accessories, we bring you the best in innovation — all in one place.</p>
                         <div className="flex items-center gap-3 mt-5">
                             {socialIcons.map((item, i) => (
                                 <Link href={item.link} key={i} className="flex items-center justify-center w-10 h-10 bg-slate-100 hover:scale-105 hover:border border-slate-300 transition rounded-full">
@@ -79,8 +79,8 @@ const Footer = () => {
                         ))}
                     </div>
                 </div>
-                <p className="py-4 text-sm text-slate-500">
-                    Copyright 2025 © gocart All Right Reserved.
+                <p className="py-4 text-sm text-slate-500 text-center">
+                    Copyright 2025 © globalmart All Right Reserved.
                 </p>
             </div>
         </footer>
