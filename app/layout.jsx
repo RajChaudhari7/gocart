eaffe8d93c362315c4762e26f2f5e1a832a88368
Raj Chaudhari <rajchaudharii1403@gmail.com>
2026-01-22 21:14:32 +0530

    updated layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,34 +1,27 @@
-import { Outfit } from "next/font/google"
-import { Toaster } from "react-hot-toast"
-import StoreProvider from "@/app/StoreProvider"
-import { ClerkProvider } from "@clerk/nextjs"
-import Navbar from "@/components/Navbar"
-import "./globals.css"
+import { Outfit } from "next/font/google";
+import { Toaster } from "react-hot-toast";
+import StoreProvider from "@/app/StoreProvider";
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
 
-const outfit = Outfit({
-  subsets: ["latin"],
-  weight: ["400", "500", "600"],
-})
+const outfit = Outfit({ subsets: ["latin"], weight: ["400", "500", "600"] });
 
 export const metadata = {
-  title: "GlobalMart. - Shop smarter",
-  description: "GlobalMart. - Shop smarter",
-}
+    title: "GlobalMart. - Shop smarter",
+    description: "GlobalMart. - Shop smarter",
+};
 
 export default function RootLayout({ children }) {
-  return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={`${outfit.className} antialiased bg-black text-white`}>
-          <StoreProvider>
-            <Navbar />
-            <main className="pt-20 pb-20">
-              {children}
-            </main>
-            <Toaster />
-          </StoreProvider>
-        </body>
-      </html>
-    </ClerkProvider>
-  )
+    return (
+        <ClerkProvider>
+            <html lang="en">
+                <body className={`${outfit.className} antialiased`}>
+                    <StoreProvider>
+                        <Toaster />
+                        {children}
+                    </StoreProvider>
+                </body>
+            </html>
+        </ClerkProvider>
+    );
 }
