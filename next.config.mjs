6983204b39e7cb5d716d07c214a357fbd69c245a
Raj Chaudhari <rajchaudharii1403@gmail.com>
2026-01-27 16:39:34 +0530

    reset next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: [
-      'ik.imagekit.io',      // ImageKit (products, reviews)
-      'images.clerk.dev',    // Clerk user avatars
-    ],
-  },
+    images:{
+        unoptimized: true
+    }
 };
 
 export default nextConfig;
