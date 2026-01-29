ae5031463b3e77b185013240e4c08001a6668f0f
Raj Chaudhari <rajchaudharii1403@gmail.com>
2026-01-29 16:31:23 +0530

    created utils files

--- components/store/StoreSidebar.jsx
+++ components/store/StoreSidebar.jsx
@@ -9,7 +9,7 @@ import {
 } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
-import { cn } from "@/lib/utils"
+import { cn } from "../lib/utils"
 
 const StoreSidebar = ({ storeInfo, pendingOrdersCount = 0 }) => {
   const pathname = usePathname()
