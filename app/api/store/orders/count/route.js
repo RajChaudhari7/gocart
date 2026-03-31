1aebc9ae966a7294e681228498cef305f2504c5e
Raj Chaudhari <rajchaudharii1403@gmail.com>
2026-03-31 13:43:03 +0530

    updated count api

--- app/api/store/orders/count/route.js
+++ app/api/store/orders/count/route.js
@@ -4,24 +4,24 @@ import { getAuth } from "@clerk/nextjs/server"
 import { NextResponse } from "next/server"
 
 export async function GET(req) {
-  try {
-    const { userId } = getAuth(req)
-    const storeId = await authSeller(userId)
+    try {
+        const { userId } = getAuth(req)
+        const storeId = await authSeller(userId)
 
-    const count = await prisma.order.count({
-      where: {
-        storeId,
-        status: {
-          not: "RETURNED" // ❌ exclude returned orders
-        }
-      }
-    })
+        const count = await prisma.order.count({
+            where: {
+                storeId,
+                status: {
+                    in: ["ORDER_PLACED"]
+                }
+            }
+        })
 
-    return NextResponse.json({ count })
-  } catch (error) {
-    return NextResponse.json(
-      { error: error.message },
-      { status: 500 }
-    )
-  }
+        return NextResponse.json({ count })
+    } catch (error) {
+        return NextResponse.json(
+            { error: error.message },
+            { status: 500 }
+        )
+    }
 }
