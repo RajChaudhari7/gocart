c14911fbe4f2d33c4e3c0b669d61f67912441f92
Raj Chaudhari <rajchaudharii1403@gmail.com>
2026-04-08 13:28:34 +0530

    updated send-otp route

--- app/api/store/send-otp/route.js
+++ app/api/store/send-otp/route.js
@@ -1,5 +1,5 @@
 import prisma from "@/lib/prisma"
-import { transporter } from "@/lib/sendEmail"
+import { transporter } from "@/lib/sendEmail.js"
 import { NextResponse } from "next/server"
 
 export async function POST(req) {
