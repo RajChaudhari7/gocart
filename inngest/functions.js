37c374871d47f290750b2f8998294c4652759328
Raj Chaudhari <rajchaudharii1403@gmail.com>
2026-04-25 14:22:54 +0530

    updated inngest function

--- inngest/functions.js
+++ inngest/functions.js
@@ -3,63 +3,75 @@ import { inngest } from "./client";
 
 // inngest function to save user data to database
 export const syncUserCreation = inngest.createFunction(
-    { id: 'sync-user-create' },
-    { event: 'clerk/user.created' },
-    async ({ event }) => {
-        const { data } = event
-        await prisma.user.create({
-            data: {
-                id: data.id,
-                email: data.email_addresses[0].email_address,
-                name: `${data.first_name} ${data.last_name}`,
-                image: data.image_url,
-            }
-        })
-    }
-)
+  {
+    id: "sync-user-create",
+    triggers: [{ event: "clerk/user.created" }],
+  },
+  async ({ event }) => {
+    const { data } = event;
 
-// inngest function to update user in database
+    await prisma.user.create({
+      data: {
+        id: data.id,
+        email: data.email_addresses[0].email_address,
+        name: `${data.first_name} ${data.last_name}`,
+        image: data.image_url,
+      },
+    });
+  }
+);
+
+// update user
 export const syncUserUpdation = inngest.createFunction(
-    { id: 'sync-user-update' },
-    { event: 'clerk/user.updated' },
-    async ({ event }) => {
-        const { data } = event
-        await prisma.user.update({
-            where: { id: data.id },
-            data: {
-                email: data.email_addresses[0].email_address,
-                name: `${data.first_name} ${data.last_name}`,
-                image: data.image_url,
-            }
-        })
-    }
-)
+  {
+    id: "sync-user-update",
+    triggers: [{ event: "clerk/user.updated" }],
+  },
+  async ({ event }) => {
+    const { data } = event;
 
-// inngest function to delete user in database
+    await prisma.user.update({
+      where: { id: data.id },
+      data: {
+        email: data.email_addresses[0].email_address,
+        name: `${data.first_name} ${data.last_name}`,
+        image: data.image_url,
+      },
+    });
+  }
+);
+
+// delete user
 export const syncUserDeletion = inngest.createFunction(
-    { id: 'sync-user-delete' },
-    { event: 'cler/user/deleted' },
-    async ({ event }) => {
-        const { data } = event
-        await prisma.user.delete({
-            where: { id: data.id }
-        })
-    }
-)
+  {
+    id: "sync-user-delete",
+    triggers: [{ event: "clerk/user.deleted" }],
+  },
+  async ({ event }) => {
+    const { data } = event;
 
-// Inngest Function to delete coupon on expiry
+    await prisma.user.delete({
+      where: { id: data.id },
+    });
+  }
+);
+
+// delete coupon on expiry
 export const deleteCouponOnExpiry = inngest.createFunction(
-    { id: 'delete-coupon-on-expiry' },
-    { event: 'app/coupon.expired' },
-    async ({ event, step }) => {
-        const { data } = event
-        const expiryDate = new Date(data.expires_at)
-        await step.sleepUntil('wait-for-expiry', expiryDate)
+  {
+    id: "delete-coupon-on-expiry",
+    triggers: [{ event: "app/coupon.expired" }],
+  },
+  async ({ event, step }) => {
+    const { data } = event;
+    const expiryDate = new Date(data.expires_at);
 
-        await step.run('delete-coupon-from-database', async () => {
-            await prisma.coupon.delete({
-                where: { code: data.code }
-            })
-        })
-    }
-)
+    await step.sleepUntil("wait-for-expiry", expiryDate);
+
+    await step.run("delete-coupon-from-database", async () => {
+      await prisma.coupon.delete({
+        where: { code: data.code },
+      });
+    });
+  }
+);
