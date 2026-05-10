a14d26ca16cda3cb277637bd133e87ebc47eea66
Raj Chaudhari <rajchaudharii1403@gmail.com>
2026-05-10 21:44:16 +0530

    created service proider

---
+++ public/sw.js
@@ -0,0 +1,48 @@
+/* /public/sw.js */
+
+const CACHE_NAME = "nandurbar-bazar-v1"
+const urlsToCache = [
+  "/",
+  "/store",
+  "/manifest.json",
+  "/store-manifest.json",
+  "/icon-192.png",
+  "/icon-512.png",
+  "/seller-192.png",
+  "/seller-512.png"
+]
+
+/* INSTALL */
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache)
+    })
+  )
+  self.skipWaiting()
+})
+
+/* ACTIVATE */
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key)
+          }
+        })
+      )
+    )
+  )
+  self.clients.claim()
+})
+
+/* FETCH */
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request)
+    })
+  )
+})
