b25a646ba0e581ae086d718e07f219ba95f418f4
Raj Chaudhari <rajchaudharii1403@gmail.com>
2026-06-06 15:07:29 +0530

    file moved

--- components/driver/DriverLayout.jsx
+++ components/driver/DriverLayout.jsx
@@ -1,35 +1,22 @@
 'use client'
 
-import { usePathname, useRouter } from "next/navigation"
 import DriverNavbar from "./DriverNavbar"
 import DriverSidebar from "./DriverSidebar"
-import { useEffect } from "react"
 
 export default function DriverLayout({ children }) {
-
-    const router = useRouter()
-
-    useEffect(() => {
-
-        const driver = localStorage.getItem("driver")
-
-        if (!driver) {
-            router.replace("/driver/login")
-        }
-
-    }, [])
-
     return (
         <div className="min-h-screen bg-slate-50">
 
             <DriverNavbar />
 
             <div className="flex">
+
                 <DriverSidebar />
 
                 <main className="flex-1 p-6">
                     {children}
                 </main>
+
             </div>
 
         </div>
