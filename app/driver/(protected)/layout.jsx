de72918ee5582c9a4a51c17f134759d34d3d0768
Raj Chaudhari <rajchaudharii1403@gmail.com>
2026-06-21 01:38:11 +0530

    updated driver

--- app/driver/(protected)/layout.jsx
+++ app/driver/(protected)/layout.jsx
@@ -1,6 +1,9 @@
 import DriverLayout from "@/components/driver/DriverLayout"
 
-
+export const metadata = {
+    title: "Nandurbar Bazar Driver",
+    manifest: "/driver-manifest.json",
+}
 
 export default function Layout({ children }) {
     return (
