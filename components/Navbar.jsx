289abb2ec8da2f55f55ae0fbbc668e868e08629d
Raj Chaudhari <rajchaudharii1403@gmail.com>
2026-06-25 18:52:08 +0530

    updated navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -152,8 +152,8 @@ const Navbar = () => {
             <Image
               src="/app.png"
               alt="Nandurbar Bazar Logo"
-              width={80}
-              height={80}
+              width={70}
+              height={70}
               className="object-contain"
               priority
             />
